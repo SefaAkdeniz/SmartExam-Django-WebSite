33d071a5a875c63af2eb42271ec83320bf98a02c
Yusuf Özgül <yozgul@icloud.com>
2019-12-06 00:15:27 +0300

    Completed Admin Panel

--- exam/admin.py
+++ exam/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from . import models
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('text','trueAnswer','category')
+    list_display_links = ('text','trueAnswer','category')
+    list_filter = ('category','trueAnswer')
+    list_per_page = 10
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_per_page = 10
+
+class Student_LogAdmin(admin.ModelAdmin):
+    list_display = ('question','user','date','answer')
+    list_display_links = ('question','user','date','answer')
+    list_filter = ('question','user','date','answer')
+    list_per_page = 10
 
 # Register your models here.
+admin.site.register(models.Category,CategoryAdmin)
+admin.site.register(models.Question,QuestionAdmin)
+admin.site.register(models.Student_Log,Student_LogAdmin)
--- exam/models.py
+++ exam/models.py
@@ -1,3 +1,31 @@
 from django.db import models
-
+import os
 # Create your models here.
+
+def get_image_path(instance, filename):
+    return 'photos/'+filename
+
+class Category(models.Model):
+    category = models.CharField(max_length=50,verbose_name='Kategori', unique=True)
+
+    def __str__(self):
+        return self.category
+
+class Question(models.Model):
+    image = models.ImageField(upload_to=get_image_path, blank=True, null=True,verbose_name='Fotograf')
+    text = models.TextField(verbose_name='Soru', unique=True)
+    trueAnswer = models.CharField(max_length=50,verbose_name='Dogru Cevap')
+    falseAnswer1 = models.CharField(max_length=50,verbose_name='Yanlis Cevap 1')
+    falseAnswer2 = models.CharField(max_length=50,verbose_name='Yanlis Cevap 2')
+    falseAnswer3 = models.CharField(max_length=50,verbose_name='Yanlis Cevap 3')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,verbose_name='Kategori')
+
+    def __str__(self):
+        return self.text
+
+class Student_Log(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE,verbose_name='Soru')
+    user = models.ForeignKey("auth.User", on_delete=models.CASCADE,verbose_name='Ogrenci')
+    date = models.DateTimeField(blank=True, null=True,verbose_name='Tarih')
+    answer = models.BooleanField(verbose_name='Dogru')
+
