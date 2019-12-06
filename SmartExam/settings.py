c8e696285cd2c5ee6af8b1b6208370cad870fc86
İsmail Sefa <ismailsefa.akdnz@gmail.com>
2019-12-07 01:09:37 +0300

    Completed Login and Register

--- SmartExam/settings.py
+++ SmartExam/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'pages',
     'exam',
+    'user',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -123,3 +124,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger'
+}
---
+++ user/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
+from django.contrib import messages
+
+# Create your views here.
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username,password=password)
+        if user is not None:
+            auth.login(request,user)
+            messages.add_message(request,messages.SUCCESS,'Oturum açıldı.')
+            return redirect('index')
+        else:
+            messages.add_message(request,messages.ERROR,'Hesap bulunamadı.')
+            return redirect('login')
+    else:
+        return render(request,'login.html')
+
+def register(request): 
+    if request.method == 'POST':       
+        username = request.POST['username']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        password = request.POST['password']
+        repassword = request.POST['repassword']
+        if password==repassword:
+            if User.objects.filter(username=username).exists():
+                messages.add_message(request,messages.WARNING,'Bu kullanıcı adı daha önce kullanılmıştır.')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username,first_name=first_name,last_name=last_name,password=password)
+                user.save()
+                messages.add_message(request,messages.SUCCESS,'Hesap başarıyla oluşturuldu.')
+                return redirect('login')
+        else:
+            messages.add_message(request,messages.ERROR,'Parlolar eşleşmiyor.')
+            return redirect('register')
+    else:
+        return render(request,'register.html')
+
+def logout(request):
+    return render(request,'logout.html')
