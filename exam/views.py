291e14b7a26bbdd52f52a5e179d075c86b9c36c5
İsmail Sefa <ismailsefa.akdnz@gmail.com>
2019-12-08 14:54:16 +0300

    Started Exam

--- exam/views.py
+++ exam/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from models import Student_Log
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib import auth
+from django.contrib import messages
+
 
 # Create your views here.
 
+@login_required(login_url="user:login")
 def index(request):
-    return render(request,'exam.html')
+    if request.method == 'POST':
+        pass
+    else:
+        count = Student_Log.objects.filter(user=request.user).count()
+        if count == 0:
+            return render(request,'exam.html')
