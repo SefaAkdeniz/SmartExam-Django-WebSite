0359fca8a0a421b21274d2c8d1c8f5d36f322561
Yusuf Özgül <yozgul@icloud.com>
2019-12-08 17:43:34 +0300

    added exam control mechanism

--- exam/views.py
+++ exam/views.py
@@ -1,18 +1,29 @@
 from django.shortcuts import render,redirect
-from models import Student_Log
+from exam.models import Student_Log,Question
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.contrib import auth
 from django.contrib import messages
+from django.utils import timezone
+from datetime import datetime, timedelta
+from django.utils import formats
 
 
 # Create your views here.
 
-@login_required(login_url="user:login")
+@login_required(login_url="login")
 def index(request):
     if request.method == 'POST':
         pass
     else:
-        count = Student_Log.objects.filter(user=request.user).count()
-        if count == 0:
+        if Student_Log.objects.filter(user=request.user).exists()==0:
+            messages.add_message(request,messages.SUCCESS,'ilk sinav')
+            sorular=
             return render(request,'exam.html')
+        if Student_Log.objects.filter(user=request.user,date__year=timezone.now().year,date__month=timezone.now().month,date__day=timezone.now().day).exists()==0:
+            messages.add_message(request,messages.SUCCESS,'normal sinav')
+            return render(request,'exam.html')           
+        else:
+            messages.add_message(request,messages.SUCCESS,'bugun sinav oldun')
+            return redirect('index')
+            
+            
